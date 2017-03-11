13bda4b7b73af52eae60356a5f23422efd43bb89
Jan Mikicki <jan.mikicki@outlook.com>
2017-03-11 17:12:14 +0100

    prace w toku wez na razie nic nie zmieniaj bo sam sie w tym strasznie gubie

--- projekt_1/projekt_1.cpp
+++ projekt_1/projekt_1.cpp
@@ -68,13 +68,28 @@ void zapisywanie_polaczenia ()
     cout<<"                Aktualnie podales "<<ilosc_polaczen<<" polaczen.\n\n\n";
 
     cout<<"                Podaj numer pierwszego miasta: ";
-    cin>> nowe->miasto1;
+	cin >> nowe->miasto1;
+
+	while (nowe->miasto1 <= 0 || nowe->miasto1 > ilosc_miast) {
+		cout << "podaj poprawny numer miasta: ";
+		cin >> nowe->miasto1;
+	};
 
    cout<<"                 Podaj numer drugiego miasta: ";
     cin>> nowe->miasto2;
 
+	while (nowe->miasto2 <= 0 || nowe->miasto2 > ilosc_miast) {
+		cout << "podaj poprawny numer miasta: ";
+		cin >> nowe->miasto2;
+	};
+
     cout<<"                   Podaj wage polaczenia: ";
     cin>> nowe->waga;
+
+	while (nowe->waga <= 0) {
+		cout << "Podaj dodatnia wage: ";
+		cin >> nowe->waga;
+	}
 
 	if (ilosc_polaczen == 0) {
 		start_listy = nowe;
@@ -137,9 +152,95 @@ void porzadkowanie()
     
 }
 
+struct drzewo {
+	polaczenie* krawedzie;
+	drzewo* nastepne_drzewo;
+};
+
+int przeszukaj_drzewo(polaczenie* krawedzie, polaczenie* aktualne) {
+	
+	// co zwraca:
+	// 0 - nie znaleziono w drzewie wierzcholkow
+	// 1 - znaleziono w drzewie jeden z wierzcholkow (moze da sie polaczyc z innym drzewem)
+	// 2 - znaleziono w drzewie oba wierzcholki (nie dolaczaj)
+	
+	int pierwszy = aktualne->miasto1;
+	int drugi = aktualne->miasto2;
+	int ktory_znaleziono;
+	polaczenie* przeszukaj = krawedzie;
+
+	do {
+	
+		if (przeszukaj->miasto1 == pierwszy || przeszukaj->miasto2 == pierwszy) {
+			if (ktory_znaleziono = 2) return 2;
+			else ktory_znaleziono = 1;
+		}
+		else if (przeszukaj->miasto1 == drugi || przeszukaj->miasto2 == drugi) {
+			if (ktory_znaleziono = 1) return 2;
+			else ktory_znaleziono = 2;
+		}
+		else ktory_znaleziono = 0;
+
+		przeszukaj = przeszukaj->nastepne;
+
+	} while (przeszukaj != NULL);
+
+	if (ktory_znaleziono == 1) return 1;
+	else if (ktory_znaleziono == 2) return 1;
+	else return 0;
+}
+
+int przeszukaj_las(drzewo* las, polaczenie* aktualne) {
+
+	bool znaleziono; // sprawdza czy wczesniej znaleziono jakis wierzcholek w ktoryms poprzednim drzewie
+	int i = 1;
+	do {
+		if (przeszukaj_drzewo(las->krawedzie, aktualne) == 2) return 2; //wyrzuc na smietnik
+		else if (przeszukaj_drzewo(las->krawedzie, aktualne) == 1) {
+			if (znaleziono) {
+				aktualne->przynaleznosc_do_drzewa = i; //lacz drzewa i oraz i-1
+				return i;
+			}
+			else {
+				znaleziono = true;
+				aktualne->przynaleznosc_do_drzewa = i;
+				las = las->nastepne_drzewo;
+			}
+		}
+		else las = las->nastepne_drzewo;
+		i++;
+		
+	} while (las != NULL);
+
+	if(znaleziono)
+	//dolacz do drzewa i
+
+	else //zrob nowe drzewo
+}
+
 void tworzenie_polaczen()
 {
-    ;
+	polaczenie* aktualne = start_listy;
+	start_listy = start_listy->nastepne;
+
+	drzewo* las = new drzewo;
+	
+	las->nastepne_drzewo = NULL;
+	las->krawedzie = aktualne;
+
+	do {
+		aktualne = start_listy;
+		start_listy = start_listy->nastepne;
+
+		if (przeszukaj_las(las, aktualne) == 0) {
+		
+		
+		}
+		else if () {}
+		else {};
+
+
+	} while (start_listy != NULL);
 }
 
 void wypisywanie()
